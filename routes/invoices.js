d61e858afce667428dc2d19e36b513c593613d86
jade-rickerts <jade@code-beta.com>
2018-05-19 13:42:16 +0200

    update invoices route - add product to invoice. update views.

--- routes/invoices.js
+++ routes/invoices.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var router = express.Router();
 var Invoice = require("../models/invoice.js");
+var Product = require("../models/product.js");
 
 //INDEX ROUTE
 router.get("/", function(req, res){
@@ -11,8 +12,8 @@ router.get("/", function(req, res){
         } else {
             res.render("invoices/index.ejs", {invoices: allInvoices});
         }
-    })
-})
+    });
+});
 
 //NEW ROUTE
 router.get("/new", function(req, res){
@@ -33,7 +34,27 @@ router.post("/", function(req, res){
         } else {
             res.redirect("/invoices/");
         }
-    })
-})
+    });
+});
+
+//ADD PRODUCTS TO INVOICE ROUTE
+router.get("/:invoiceId/addProducts", function(req, res){
+    Invoice.findById(req.params.invoiceId, function(err, foundInvoice){
+        if (err) {
+            console.log(err);
+        } else {
+            Product.find({}, function(err, allProducts){
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.render("invoices/products.ejs", {
+                        invoice: foundInvoice,
+                        products: allProducts
+                    });
+                }
+            });
+        }
+    });
+});
 
 module.exports = router;
